39dba18377ddcbd5831878d7ce6171d0044e2f49
shiyi <shiyi.ntt@gmail.com>
2022-04-22 03:34:37 +0200

    319

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.sass']
 })
-export class ProductsComponent implements OnInit {
+export class ProductsComponent {
+  products$: any;
 
-  constructor() { }
+  constructor(productService: ProductService) {
+    this.products$ = productService.getAll();
+  }
 
   ngOnInit(): void {
   }
