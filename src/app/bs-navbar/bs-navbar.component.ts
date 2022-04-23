d440d70e3efa910335e48de16988884d430a5a27
shiyi <shiyi.ntt@gmail.com>
2022-04-23 03:02:40 +0200

    336

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,20 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs';
 import { AuthService } from '../auth.service';
 import { AppUser } from '../models/app-user';
+import { ShoppingCartService } from '../shopping-cart.service';
 
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.sass']
 })
-export class BsNavbarComponent {
+export class BsNavbarComponent implements OnInit {
   appUser: AppUser | undefined;
+  shoppingCartItemCount!: number;
 
-  constructor(private auth: AuthService) {
-    auth.appUser$.subscribe(appUser => this.appUser = appUser);
+  constructor(private auth: AuthService, private shoppingCartService: ShoppingCartService) {
+
   }
 
-  ngOnInit(): void {
+  async ngOnInit() {
+    this.auth.appUser$.subscribe(appUser => this.appUser = appUser);
+
+    let cart$ = await this.shoppingCartService.getCart();
+    // cart$.snapshotChanges().pipe(
+    //   map((x:any) => {
+    //     const items = x.payload.val().items;
+    //     console.log(items)
+    //     return items;
+    //   })
+    // ).subscribe(cart => {
+    //   this.shoppingCartItemCount = 0;
+    //   for (let productId in cart) {
+    //     this.shoppingCartItemCount += cart[productId].quantity;
+    //   }
+    // })
+    cart$.snapshotChanges().pipe(
+      map((x:any) => {
+        const items = x.payload.val();
+        console.log(items)
+        return items;
+      })
+    ).subscribe(cart => {
+      this.shoppingCartItemCount = 0;
+      for (let productId in cart.items) {
+        this.shoppingCartItemCount += cart.items[productId].quantity;
+      }
+    })
   }
 
   logout() {
