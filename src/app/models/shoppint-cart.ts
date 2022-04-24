3a0fc3367dc83cd298cf63bc48a401089e17d3f2
shiyi <shiyi.ntt@gmail.com>
2022-04-24 18:35:30 +0200

    340

--- src/app/models/shoppint-cart.ts
+++ src/app/models/shoppint-cart.ts
@@ -1,7 +1,13 @@
 import { ShoppingCartItem } from "./shopping-cart-item";
 
 export class ShoppingCart {
-  constructor(public items: ShoppingCartItem[]){}
+  items: ShoppingCartItem[] = [];
+  constructor(public itemsMap: { [productId: string]: ShoppingCartItem} ){
+    for(let productId in itemsMap) {
+      let item = itemsMap[productId]
+      this.items.push(new ShoppingCartItem(item.product, item.quantity));
+    }
+  }
 
   get totalItemsCount() {
     let count = 0;
@@ -9,5 +15,13 @@ export class ShoppingCart {
       count += this.items[productId].quantity;
     }  
     return count;
+  }
+
+  get totalPrice() {
+    let sum = 0;
+    for(let productId in this.items) {
+      sum += this.items[productId].totalPrice;
+    }
+    return sum;
   }
 }
